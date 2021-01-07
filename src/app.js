a3f173de4a82280d7d3856530448a0384e0fadf4
Matthew Coker <mattacoker@gmail.com>
2021-01-06 19:52:15 -0500

    Activity Indicator/Spinner.js is funky on android and LogOut Styling wont show up properly without height

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Button, Header, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {loggedIn: null};
+
   componentWillMount() {
     if (!firebase.apps.length) {
       firebase.initializeApp({
@@ -18,13 +20,34 @@ class App extends Component {
     } else {
       firebase.app(); // if already initialized, use that one
     }
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{height: 105}}>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
